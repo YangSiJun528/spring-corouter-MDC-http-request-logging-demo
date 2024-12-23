196c7f8525ff1d0a4ea9d4dd02e75a6ef054190a
sijun-yang <yangsijun5528@gmail.com>
2024-12-23 15:41:27 +0900

    쓸모없는 로깅 제거

--- src/main/kotlin/dev/joon/corouter/request_id/logging/DemoRouter.kt
+++ src/main/kotlin/dev/joon/corouter/request_id/logging/DemoRouter.kt
@@ -1,6 +1,5 @@
 package dev.joon.corouter.request_id.logging
 
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
@@ -8,15 +7,13 @@ import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
 class DemoRouter {
-    private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun route(handler: DemoHandler) = coRouter {
-        log.info("Initializing router configuration")
         path("/demo").nest {
             accept(MediaType.APPLICATION_JSON).nest {
                 GET("/test", handler::get)
             }
         }
-    }.also { log.info("Router configuration completed") }
+    }
 }
